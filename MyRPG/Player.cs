bce546ea1e051e709a4ce2ec02ac4d60ee6ad592
KR1PT1CS <mogos2625727@gmail.com>
2025-04-04 14:27:37 +0100

    Possibly finished the player.cs code, will do a quick review

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -3,5 +3,48 @@
 namespace MyRPG
 {
     public class Player
-        private static void Main()
-        {
+    {
+        private int xp;
+         private float health;
+         
+         public string Name { get; }
+         
+         public int XP
+         {
+         public int Level
+             get => 1 + (xp / 1000);
+         }
+ 
+         public float Health
+         {
+             get => health;
+             set
+             {
+                 health = value;
+                 if (health < 0) health = 0;
+             }
+         }
+         
+         public float MaxHealth
+         {
+             get => 100f + (Level - 1) * 20f;
+         }
+ 
+         public Player(string name)
+         {
+             
+             this.Name = name;
+             xp = 0;
+             Health = MaxHealth;
+         }
+ 
+         public void TakeDamage(float damage)
+         {
+             
+             Health -= damage;
+             xp += (int)damage / 20;
+         }
+ 
+     }
+}
+     
